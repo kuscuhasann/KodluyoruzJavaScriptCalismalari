e7e8699482aaedc908800871f558a80e134d93b5
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-10 12:40:11 +0300

    Metinin tamamımı küçültmek büyültmek metin bu degeri içeriyormu kontrolü yapmak
    toUpperCase();
    toLowerCase();
    includes();

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("merhaba dunya")
+//Bu işlemler querySelector ile tek seferde yapılabilir 
+//erişilen deger okunabilir ve degistirilebilir
+let title =document.getElementById('title');
+console.log(title);
