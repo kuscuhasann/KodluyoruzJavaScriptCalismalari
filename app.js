7df1eafea3ea81179f4a7d92f8df5eb257840e4d
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-11 11:47:06 +0300

    Js kısmında liste elemanlarının çekilmesi, değiştirilmesi ve yeni eleman eklenmesi

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let fullName=prompt("Lütfen Adınızı giriniz : ");
-
-let title=document.querySelector("#title");
-
-title.innerHTML=`${title.innerHTML} <small>${fullName}</small>`
+//listedeki birinci elemanı çekmek ve console ekranına yazdırmak
+let ilkListeElamani=document.querySelector("ul#liste>li:first-child");
+console.log(ilkListeElamani);
+//listedeki  son elemanı çekmek ve console ekranına yazdırmak
+let sonListeElemani=document.querySelector("ul#liste>li:last-child");
+console.log(sonListeElemani);
+//listedeki son elamanın degerini değiştirmek
+sonListeElemani.innerHTML="degisen son eleman degeri ";
+//Ul'nin çekilmesi
+let ulDom=document.querySelector("ul#liste");
+//yeni bir element oluşturmak
+let liDom=document.createElement('li');
+//oluşturulan öğenin içerisine deger atamak
+liDom.innerHTML="Oluşturulan yeni öğe";
+//Oluşturulan öğenin Ul'nin sonuna eklenmesi
+ulDom.append(liDom);
+//oluşturulan öğrenin listenin başına eklenmesi
+ulDom.prepend(liDom);
