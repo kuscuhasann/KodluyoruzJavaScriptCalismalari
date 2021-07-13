a85b7c83246ba96a38bff554578c28292eb3819d
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-13 14:48:57 +0300

    Site aldığı bilgileri listeye ekleyen program.
    Site üzerinden aldığı bilgileri listeye ekleyen ve kullanıcıya gösteren bir program yapıldı.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,40 @@
-let user={userName:"hasanKuscu",isActive:true};
+let userFormDom=document.querySelector("#userForm");
 
-localStorage.setItem("userInfo",JSON.stringify(user));
+userFormDom.addEventListener("submit",submitForm);
+const alertDom=document.querySelector("#alert");
 
-let userInfo=JSON.parse(localStorage.getItem("userInfo"));
 
-console.log(userInfo);
+const ALERT= (title,mesaj,type="warning")=>`
+<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${mesaj}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+
+
+function submitForm(event)
+{
+    event.preventDefault();
+    const userNameDom=document.querySelector("#username");
+    const scoreDom=document.querySelector("#score");
+    if(userNameDom.value && scoreDom.value){
+    addItem(userNameDom.value,scoreDom.value);
+    userNameDom.value="";
+    scoreDom.value="";
+    }
+    else
+    alertDom.innerHTML=ALERT("Bilgilendirme:","Eksik Bilgi Girdiniz","warning");
+}
+
+let ulDom=document.querySelector("#userList");
+function addItem(userName,score)
+{
+    let liDom=document.createElement('li');
+    liDom.innerHTML=`
+    ${userName}
+    <span class="badge bg-primary rounded-pill">${score}</span>
+    `;
+    liDom.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    ulDom.append(liDom);
+}
+
