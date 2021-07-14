8cb4deda7bef7a408e22c7a19656b36fc2a3f940
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2021-07-14 18:00:43 +0300

    Forech Kullanımı

--- js/app.js
+++ js/app.js
@@ -1,40 +1,7 @@
-let userFormDom=document.querySelector("#userForm");
+const numbers = [12, 24, 36];
+const newArray = []; 
 
-userFormDom.addEventListener("submit",submitForm);
-const alertDom=document.querySelector("#alert");
-
-
-const ALERT= (title,mesaj,type="warning")=>`
-<div class="alert alert-${type} alert-dismissible fade show" role="alert">
-  <strong>${title}</strong> ${mesaj}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-`
-
-
-function submitForm(event)
-{
-    event.preventDefault();
-    const userNameDom=document.querySelector("#username");
-    const scoreDom=document.querySelector("#score");
-    if(userNameDom.value && scoreDom.value){
-    addItem(userNameDom.value,scoreDom.value);
-    userNameDom.value="";
-    scoreDom.value="";
-    }
-    else
-    alertDom.innerHTML=ALERT("Bilgilendirme:","Eksik Bilgi Girdiniz","warning");
-}
-
-let ulDom=document.querySelector("#userList");
-function addItem(userName,score)
-{
-    let liDom=document.createElement('li');
-    liDom.innerHTML=`
-    ${userName}
-    <span class="badge bg-primary rounded-pill">${score}</span>
-    `;
-    liDom.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-    ulDom.append(liDom);
-}
-
+ numbers.forEach((numbers) => {
+     newArray.push(numbers+2);
+ })
+ console.log(newArray);
